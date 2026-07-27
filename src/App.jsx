b6c1cd52290b2ea32b64cbe23329c9a73823f355
mateusleitao <mateuselpidio0@gmail.com>
2026-07-27 12:17:52 -0300

    First commit, project created and initialized

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import styles from './App.css'
+
+function App() {
+
+  return (
+    <>
+     <body>
+      <div className={styles.bodyContainer}>a</div>
+     </body>
+    </>
+  )
+}
+
+export default App
