87a82578dfbb1a4aa9addbdcaa08180b12d6ed28
mateusleitao <mateuselpidio0@gmail.com>
2026-07-28 10:35:46 -0300

    feat: API implemented and tested, working

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,6 +1,17 @@
+import { useEffect, useState } from "react";
 import styles from "./Card.module.css";
+import axios from "axios";
 
 function Card() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/users")
+      .then((res) => setData(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <>
       <main className={styles.cardContainer}>
@@ -13,7 +24,11 @@ function Card() {
           <button className={styles.insertBtn}>Add todo</button>
         </header>
         <div className={styles.todoListContainer}>
-          <ul className={styles.todoList}><li>a</li></ul>
+          <ul className={styles.todoList}>
+            {data.map((api, key) => (
+              <li key={api.id}>{api.name}</li>
+            ))}
+          </ul>
         </div>
       </main>
     </>
